d9ce76a79a956eb90c51424c01479d9c27fea128
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-12 22:13:58 -0700

    Init commit: Set up express and webpack : still need to add code to render index.html

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    mode: 'development',
+
+    entry : {
+        app: './src/App.js'
+    } ,
+
+    output: {
+
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        publicPath: "/dist",
+        sourceMapFilename: "public/build/bundle.map"
+    },
+
+
+    //used to map where errors occur in bundle
+    devtool: "#source-map",
+
+
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+
+                        presets: ["react",'es2015']
+                    }
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+
+            {
+                test: /\.(png|jpg|gif)$/,
+
+                //keep the og name and the ext
+                //outputPath : copy temp and place here && update
+                // places imgs into the html file : the final dir before img get bundled
+
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            }
+            ]
+    },
+
+    // Plugins
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My Custom HTML Temp',
+        template: './public/theme/index.html'
+    })
+    ]
+
+
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'dist'),
+    //     compress: true,
+    //     port: 3000
+    // }
+};
+
+
+
