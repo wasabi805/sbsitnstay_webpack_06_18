cbe02835faca9d9a820e102aba064787aa0f09f4
‘Tim <timothy.j.ocampo@gmail.com>
2018-07-19 14:20:13 -0700

    https://www.youtube.com/watch?v=d6mv4BrXMiM&lc=z23oxrbbksy2unkqs04t1aokg5eyj4lsjwrmmyv4tk4cbk0h00410.1532032074573271 : OnHOVER EVENTS: Removed the buttons from services: if you hover over a section, with will pull up the corresponding text

---
+++ src/components/landing/services-text.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+
+const ServiceText =(props)=>{
+
+    if(props.isHovered && props.isWalking){
+        console.log(props, '1+1=2');
+
+       return(
+
+           <div id='landing-content'>
+              <p>
+                  'I grew up on the crime side, the New York Times side\n' +
+                  'Staying alive was no jive\n' +
+                  'Had second hands, moms bounced on old men\n' +
+                  'So then we moved to Shaolin land\n' +
+                  'A young youth, yo rockin\' the gold tooth, \'Lo goose\n' +
+                  'Only way, I begin to G off was drug loo'
+              </p>
+           </div>
+       )
+    }
+
+    if(props.isHoveredFeeding && props.isFeeding){
+        return(
+
+            <div className='landing-content'>
+
+                <p>"Cause day and night, The lonely stoner seems to free his mind at night,
+                    He's all alone somethings will never change,
+                    The lonely loner seems to free his mind at night
+                    (at, at, at night)"</p>
+            </div>
+        )
+    }
+
+    if(props.isHoveredOverNight && props.isOverNight){
+       return(
+           <div className='landing-content'>
+               <p>
+                   Last night took an L, but tonight I bounce back ,
+                   Wake up every morning, by the night, I count stacks,
+                   Knew that ass was real when I hit, it bounce back'
+               </p>
+           </div>
+       )
+    }
+
+
+
+    else{
+        return null
+    }
+
+
+};
+
+export default ServiceText
+
+
--- src/components/landing/services.js
+++ src/components/landing/services.js
@@ -3,6 +3,11 @@ import {Link} from 'react-router-dom';
 
 import {DropdownTrigger, DropdownContent} from 'react-simple-dropdown'
 
+import ServicesText from './services-text'
+
+
+
+
 import dogLaptop from '../../images/dog-laptop.jpg'
 import pupsEating from '../../images/pups-eating.jpg'
 import dogNap from '../../images/dog-sleeping-01.jpg'
@@ -14,80 +19,57 @@ class Services extends Component{
         super(props);
         this.state={
 
-            showWalkingText: true,
-            showFeedingText: true,
-            showOvernightCareText: true,
-            showDropinsText: true,
-            showAdventuresText: true,
+            isHovered: false,
+            isHoveredFeeding: false,
+            isHoveredOverNight: false
 
 
-        }
+        };
 
-        this.handleShowWalkingText = this.handleShowWalkingText.bind(this);
-        this.handleShowFeedingText = this.handleShowFeedingText.bind(this);
-        this.handleOvernightCareText = this.handleOvernightCareText.bind(this);
-        this.handleShowDropinsText = this.handleShowDropinsText.bind(this);
-        this.handleAdventuresText = this.handleAdventuresText.bind(this);
-        this.onClick = this.onClick.bind(this);
+        //HOVERED
+        this.onMouseOver = this.onMouseOver.bind(this);
+        this.onMessageHover = this.onMessageHover.bind(this);
+        this.onFeedingHover= this.onFeedingHover.bind(this);
+        this.onOverNightHover= this.onOverNightHover.bind(this);
 
+    }
+    //listen for hover
+    onMouseOver(){
 
     }
 
+    //render msg
+    onMessageHover(event){
 
-    handleShowWalkingText(){
-        console.log('Show Walking Text');
+        console.log('this msg: ', event.target, ' was just hovered');
 
-        this.setState(prevState =>({
-            showWalkingText: !prevState.showWalkingText
-        }));
+        this.setState({
+            isHovered: true,
+            isHoveredFeeding: false,
+            isHoveredOverNight: false,
+        });
 
-
-
-        console.log(this.state.showWalkingText, '<-------');
-        console.log(this.state.showFeedingText, '<---show feeding----');
-
-    }
-
-    handleShowFeedingText(){
-        console.log('Show Feeding Text');
-
-        this.setState(prevState =>({
-            showFeedingText: !prevState.showFeedingText
-        }))
-
-        console.log(this.state.showFeedingText, '<|||||||');
+        console.log('state msg is now ; isHovered', this.state.isHovered);
 
     }
 
-    handleOvernightCareText(){
-        console.log('Show Overnight Text');
+    onFeedingHover(event){
+        console.log(event.target, 'Feeding');
 
-        this.setState(
-            prevState =>({showOvernightCareText: !prevState.showOvernightCareText})
-
-        )
-
+        this.setState({
+            isHoveredFeeding: true,
+            isHoveredOverNight: false,
+            isHovered: false
+        })
     }
 
-    handleShowDropinsText(){
-        console.log('Show Dropin  Text?');
-        this.setState(prevState =>({
-            showDropinsText: !prevState.showDropinsText
-        }))
-
-    }
-
-    handleAdventuresText(){
-        this.setState(prevState =>({
-            showAdventuresText: !prevState.showAdventuresText
-        }))
-    }
-
-
-
-    onClick(e){
-        console.log(e);
-
+    onOverNightHover(event){
+        console.log(event.target, 'Overnight');
+        this.setState({
+            isHoveredOverNight: true,
+            isHovered: false,
+            isHoveredFeeding: false,
+        })
     }
 
 
@@ -98,7 +80,9 @@ class Services extends Component{
 
                 {/*SERVICES*/}
 
-                <section id="landing-section">
+                <section id="landing-section"
+                         onMouseOver={this.onMessageHover}
+                >
                     <div className='row'>
                         <div className='col-md-4'>
                             <div className='landing-item'>
@@ -106,41 +90,31 @@ class Services extends Component{
                             </div>
                         </div>
 
-                        <div className='col-md-8 '>
+
+                        {/*DOM elem that used as trigger*/}
+                        <div className='col-md-8 '
+                        >
                             <div className='row-md-12'>
                                 <h2>Walking</h2>
                             </div>
 
                             <div className='row-md-12'>
-                                <div className='landing-content'>
-                                    {this.state.showWalkingText ? '.' : 'I grew up on the crime side, the New York Times side\n' +
-                                        'Staying alive was no jive\n' +
-                                        'Had second hands, moms bounced on old men\n' +
-                                        'So then we moved to Shaolin land\n' +
-                                        'A young youth, yo rockin\' the gold tooth, \'Lo goose\n' +
-                                        'Only way, I begin to G off was drug loo'}
-                                </div>
-                            </div>
 
-                            <div className='row-md-12'>
-                                <div className='landing-btn'>
-                                    <button
-                                        onClick={this.handleShowWalkingText}
-
-                                    >Click Me </button>
-                                </div>
+                                <ServicesText
+                                    isHovered={this.state.isHovered}
+                                    isWalking={true}
+                                />
 
                             </div>
                         </div>
-
-
-
                     </div>
                 </section>
 
 
                 {/*FEEDING*/}
-                <section id="landing-section">
+                <section id="landing-section"
+                         onMouseOver={this.onFeedingHover}
+                >
                     <div className='row'>
                         <div className='col-md-4'>
                             <div className='landing-item'>
@@ -148,36 +122,31 @@ class Services extends Component{
                             </div>
                         </div>
 
-                        <div className='col-md-8 '>
+                        {/*DOM elem that used as trigger*/}
+                        <div className='col-md-8 '
+                        >
                             <div className='row-md-12'>
                                 <h2>FEEDING</h2>
                             </div>
 
                             <div className='row-md-12'>
-                                <div className='landing-content'>
-                                    {this.state.showFeedingText ? '.' : "Cause day and night, The lonely stoner seems to free his mind at night, He's all alone somethings will never change, The lonely loner seems to free his mind at night (at, at, at night)"}
-                                </div>
-                            </div>
 
-                            <div className='row-md-12'>
-                                <div className='landing-btn'>
-                                    <button
-                                        onClick={this.handleShowFeedingText}
-
-                                    >Click Me </button>
-                                </div>
-
+                                {/*Render COMP WHEN triggered*/}
+                                <ServicesText
+                                    isHoveredFeeding={this.state.isHoveredFeeding}
+                                    isFeeding={true}
+                                />
                             </div>
                         </div>
-
-
 
                     </div>
                 </section>
 
 
                 {/*OVERNIGHT CARE*/}
-                <section id="landing-section">
+                <section id="landing-section"
+                         onMouseOver={this.onOverNightHover}
+                >
                     <div className='row'>
                         <div className='col-md-4'>
                             <div className='landing-item'>
@@ -185,26 +154,22 @@ class Services extends Component{
                             </div>
                         </div>
 
-                        <div className='col-md-8 '>
+                        {/*DOM elem that used as trigger*/}
+                        <div className='col-md-8 '
+
+                        >
                             <div className='row-md-12'>
                                 <h2>OVERNIGHT CARE</h2>
                             </div>
 
                             <div className='row-md-12'>
-                                <div className='landing-content'>
-                                    {this.state.showOvernightCareText ? '.' : 'Last night took an L, but tonight I bounce back ,Wake up every morning, by the night, I count stacks, Knew that ass was real when I hit, it bounce back'}
-                                </div>
+                                <ServicesText
+                                    isOverNight={true}
+                                    isHoveredOverNight={this.state.isHoveredOverNight}
+                                />
                             </div>
 
-                            <div className='row-md-12'>
-                                <div className='landing-btn'>
-                                    <button
-                                        onClick={this.handleOvernightCareText}
 
-                                    >Click Me </button>
-                                </div>
-
-                            </div>
                         </div>
 
 
