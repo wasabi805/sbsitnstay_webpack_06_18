519f3ae394fc92c49413f699d7a703747a1971a6
‘Tim <timothy.j.ocampo@gmail.com>
2018-07-20 15:12:47 -0700

    connected to mLab back end: name of db is sbsitnstaywithwebpackfinal: created config/keys.js ==> contains mongoDB user name and pw: added this file to .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express= require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//used for DB Config
+const db = require('./config/keys').mongoURI;
+
+
+//used to CONNECT TO MONGO DB
+mongoose.connect(db)
+    .then(()=>console.log('MongoDB is connected!'))
+    .catch(err=>console.log(err));
+
+
 
 app.get('/', (req, res)=>res.send('Hello Buddy'));
 
