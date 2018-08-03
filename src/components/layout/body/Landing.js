27e21e1d0520e1295da86bdddcfd032b3d5c3ee5
‘Tim <timothy.j.ocampo@gmail.com>
2018-08-03 13:55:11 -0700

    Fixed the landing tiles: Now all tiles four tiles on the landing page located under the banner and nav menu line up correctly using bootstrap classes for the grid.

--- src/components/layout/body/Landing.js
+++ src/components/layout/body/Landing.js
@@ -6,37 +6,32 @@ class Landing extends Component{
     render(){
         return(
 
-                <div className='landing-main row bg-primary p-0 m-0'>
+                <div className='landing-main container-fluid bg-primary p-0 m-0'>
 
-                    <div className='landing-row row bg-success '>
+                    <div className='landing-row row bg-success'>
 
                         {/* -----   -----   ABOUT US    -----   -----   */}
-                        <div className='landing-tile col-5 '>
-                            <img src="https://res.cloudinary.com/ocampot/image/upload/v1533245189/tmnt_2003_group_selfie_by_autobot2.jpg" alt=""/>
+                        <div className='landing-tile col-sm-5 p-0'>
+                            <img className='img-fluid' src="https://res.cloudinary.com/ocampot/image/upload/v1533245189/tmnt_2003_group_selfie_by_autobot2.jpg" alt=""/>
                         </div>
 
 
 
                         {/* -----   -----   SERVICES    -----   -----   */}
-                        <div className='landing-tile col-7 '>
-                            <img src="https://res.cloudinary.com/ocampot/image/upload/v1533245189/tmnt_2003_group_selfie_by_autobot2.jpg" alt=""/>
+                        <div className='landing-tile col-sm-7 p-0'>
+                            <img className='' src="https://res.cloudinary.com/ocampot/image/upload/v1532397698/sbsitnstay/dogs_on_leash.jpg" alt=""/>
                         </div>
-                    </div>
 
-
-
-                    <div className='landing-row row '>
 
                         {/* -----   -----   GALLERY    -----   -----   */}
-                        <div className='landing-tile col-7 '>
-                            <img src="https://res.cloudinary.com/ocampot/image/upload/v1532398583/sbsitnstay/dog_leash.jpg" alt=""/>
+                        <div className='landing-tile col-sm-6 p-0'>
+                            <img className='' src="https://res.cloudinary.com/ocampot/image/upload/v1532398583/sbsitnstay/dog_leash.jpg" alt=""/>
                         </div>
-
 
 
                         {/* -----   -----   CONTACT    -----   -----   */}
-                        <div className='landing-tile col-5 '>
-                            <img src="https://res.cloudinary.com/ocampot/image/upload/v1533245189/tmnt_2003_group_selfie_by_autobot2.jpg" alt=""/>
+                        <div className='landing-tile col-sm-6 p-0'>
+                            <img className='' src="https://res.cloudinary.com/ocampot/image/upload/v1532398723/sbsitnstay/dog_laptop.jpg" alt=""/>
                         </div>
                     </div>
                 </div>
